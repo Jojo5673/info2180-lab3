2fc26e07d3a2f13f985c6c57e3d68facb9abffd5
Jojo5673 <judoudo2005@gmail.com>
2025-10-17 18:57:59 -0500

    Added hovering

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,23 @@
+let turn = 0
+
 document.addEventListener("DOMContentLoaded", () => {
-    const board = document.getElementById("board")
-    let squares = board.getElementsByTagName("div")
-    
-    for (let square of squares) {
-        square.className = "square"
-        console.log(square.style)
-    }
-}
-)
+	const board = document.getElementById("board");
+	let squares = board.getElementsByTagName("div");
+
+	for (let square of squares) {
+		square.className = "square";
+		square.addEventListener("click", () => {
+            letter = turn==1?'O':'X'
+			square.textContent=letter
+            square.classList.add(letter);
+            turn = (turn+1)%2
+		});
+        square.addEventListener("mouseenter", ()=>{
+            square.classList.add("hover")
+        })
+        square.addEventListener("mouseleave", ()=>{
+            square.classList.remove("hover")
+        })
+		console.log(square.style);
+	}
+});
